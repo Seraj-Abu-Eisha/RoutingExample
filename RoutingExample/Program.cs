0ff2c5fbaa30b203ad50908a5ad6eac3118872dd
Seraj-Abu-Eisha <serajabueisha@gmail.com>
2025-09-02 23:38:46 +0300

    ,

--- RoutingExample/Program.cs
+++ RoutingExample/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.Map("files/{filename}.{extenson}", async context =>
+{
+   string? filename = Convert.ToString(context.Request.RouteValues["filename"]);
+    await context.Response.WriteAsync("In files");
+    string? extension = Convert.ToString(context.Request.RouteValues["extension"]);
+
+    await context.Response.WriteAsync($"In files - {filename} - {extension}");
+});
+
 app.Map("map1", async (context) => {
     await context.Response.WriteAsync("In map 1");
 });
@@ -8,6 +17,14 @@ app.Map("map2", async (context) => {
     await context.Response.WriteAsync("In map 2");
 });
 app.MapFallback (async (context) =>{
-    await context.Response.WriteAsync("Home Page");
+    await context.Response.WriteAsync($"Request at {context.Request.Path}");
+});
+
+app.Map("employee/profile/{employeename}", async context =>
+{
+    string? employeeName = Convert.ToString
+        (context.Request.RouteValues["employeename"]);
+    await context.Response.WriteAsync
+        ($"In Employee profile - {employeeName}");
 });
 app.Run();
--- RoutingExample/obj/Debug/net9.0/RoutingExample.AssemblyInfo.cs
+++ RoutingExample/obj/Debug/net9.0/RoutingExample.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RoutingExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+100f088cb45b69f9c6dad01018b0f7a2b7c8c792")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoutingExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RoutingExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
