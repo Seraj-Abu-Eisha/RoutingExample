89baa09ab7504781b76345c4c6bd9c2ff4585c6f
Seraj-Abu-Eisha <serajabueisha@gmail.com>
2025-09-08 10:52:13 +0300

    a

--- RoutingExample/obj/Debug/net9.0/RoutingExample.AssemblyInfo.cs
+++ RoutingExample/obj/Debug/net9.0/RoutingExample.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RoutingExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19a614302498f46e74bbe13eb14ea46139ce1af7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f02edca66ae4d488a2bd6c6d2bd2b7fc91819a15")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoutingExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RoutingExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
